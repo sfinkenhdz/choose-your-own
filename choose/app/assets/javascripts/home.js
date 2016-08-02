83c779f3698913ff06750aff64d7a3f84c0921e9
sfinkenhdz <sarah.finken@gmail.com>
2016-08-02 12:26:28 -0500

    Add visited? incrementing functionality

--- choose/app/assets/javascripts/home.js
+++ choose/app/assets/javascripts/home.js
@@ -10,14 +10,6 @@ $(document).ready(function(){
       event.preventDefault();
       var url = $(this).attr("action");
       var method = $(this).attr("method");
-      var choiceID = url.split("/")[2];
-      // var outcome = $(this).parent().parent().text();  GETS THE TEXT - I need the variable
-      // if i know the choice, i can get to the outcome. Does a choice have only one preceding oucome? YES //
-      // i need the outcome_id
-      // choice.previous_outcome.id
-      console.log(choiceID);
-      console.log(url);
-      console.log(method);
       $.ajax({
         method: method,
         url: url
