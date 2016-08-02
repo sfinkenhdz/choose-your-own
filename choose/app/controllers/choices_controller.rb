2df566ce413eab4b949e2f59d1e4d65ab5854135
sfinkenhdz <sarah.finken@gmail.com>
2016-08-02 10:31:46 -0500

    Add choice.set_result method

--- choose/app/controllers/choices_controller.rb
+++ choose/app/controllers/choices_controller.rb
@@ -1,11 +1,15 @@
 class ChoicesController < ActionController::Base
 
   def show
+    # Article.includes(:category, :comments)
     @choice = Choice.find(params[:id])
+    @result = @choice.set_result
+
     respond_to do |format|
       format.html
       format.json{
         render :json => @choice.to_json
+        render :json => @result.to_json
       }
     end
   end
--- choose/app/models/choice.rb
+++ choose/app/models/choice.rb
@@ -2,4 +2,14 @@ class Choice < ActiveRecord::Base
   has_many :pre_choices
   has_many :previous_outcomes, through: :pre_choices, source: :outcome
   belongs_to :resulting_outcome, class_name: "Outcome"
+
+  def set_result
+    if self.resulting_outcome.visited? == 0
+      @result = self.resulting_outcome
+    else
+      @result = self.alt_resulting_outcome
+    end
+  end
+
+
 end
